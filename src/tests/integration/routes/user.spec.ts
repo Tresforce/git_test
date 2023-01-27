3cbbfd3b9422b3a1089bd842821701a788cf5765
James Davidson <57471573+Tresforce@users.noreply.github.com>
2023-01-27 12:59:28 -0800

    Initial commit

---
+++ src/tests/integration/routes/user.spec.ts
@@ -0,0 +1,11 @@
+/**
+ * Integration Tests for user routes
+ * @group integration
+ */
+
+describe('user route', () => {
+  test.todo('should create user');
+  test.todo('should update user');
+  test.todo('should disable user');
+  test.todo('should delete user');
+});
